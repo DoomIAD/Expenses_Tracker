ca70134336dbe1cbb0b5e22deb0a43bcd114b941
DoomIAD <97483205+DoomIAD@users.noreply.github.com>
2026-05-08 14:50:16 -0500

    - Added Flask based GUI - Added basic homepage - Added basic page to add spreadsheets (with error handling)
    TODO
    - Implement edit_merchants
    - implement expenditures.html

--- main.py
+++ main.py
@@ -1,14 +1,48 @@
-from scripts.sheet_importer import *
+from flask import Flask, render_template, request, redirect, url_for
 
-user_action=input("What would you like to do today? (please input the number)\n1. upload Google Sheet link\n2. Modify category DB\n")
-if user_action=="1":
-    sheet_url=input("Please input the URL for the google sheet:\n")
-    sheet_import(sheet_url)
-elif user_action=="2":
-    print("Current catorgorized vendors:\n")
-    print_table("merchants")
-    vendor=input("\n=======================\nWho is the vendor?\n")
-    category=input("\nCategories: groceries, restaurant, transport, shopping, entertainment, utilities, subscriptions, health, travel, other\n")
-    update_merchant(vendor,category)
-    
-    
+from scripts.sheet_importer import sheet_import
+
+app = Flask(__name__)
+
+# Home Page
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+# New Google Sheet
+@app.route("/add_spreadsheet", methods=["GET", "POST"])
+def add_spreadsheet():
+    success = False
+    failure = False
+
+    # Pulls the URL from the form to be used in sheet_import()
+    if request.method == "POST":
+        sheet_url = request.form["url"]
+        try:
+            sheet_import(sheet_url)
+            success = True
+        except Exception as e:
+            print("Error:", e)
+            failure = True
+
+    # Tells the site if success or failure
+    return render_template(
+        "add_spreadsheet.html",
+        success=success,
+        failure=failure
+    )
+
+
+# Modify Merchants
+@app.route('/edit_merchants')
+def edit_merchants():
+    return render_template("edit_merchants.html")
+
+# Spending visualizations
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+# Run the Flask app
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
